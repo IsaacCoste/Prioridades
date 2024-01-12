88d242dc64890509946d7b1f8de6dd9c6aac4ff6
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2024-01-12 17:20:00 -0400

    Creacion del modelo y el contexto

---
+++ Practica_Prioridades/Practica_Prioridades/DAL/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Practica_Prioridades.Models;
+
+namespace Practica_Prioridades.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options){ }
+        public DbSet<Prioridades> Prioridades { get; set; }
+    }
+}
---
+++ Practica_Prioridades/Practica_Prioridades/Models/Prioridades.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Practica_Prioridades.Models
+{
+    public class Prioridades
+    {
+        [Key]
+        public int PrioridadId { get; set; }
+        [Required(ErrorMessage = "Debe Completar")]
+        public string? Descripción { get; set; }
+        public int DiasCompromiso { get; set; }
+    }
+}
