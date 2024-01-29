f85b805f08e7ba8da357caba11594cd4b938d9a3
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2024-01-29 02:38:14 -0400

    Hice la migracion de la base de datos

---
+++ Practica_Prioridades/Practica_Prioridades/Migrations/20240129063523_Sistemas.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Practica_Prioridades.Migrations
+{
+    /// <inheritdoc />
+    public partial class Sistemas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sistemas",
+                columns: table => new
+                {
+                    SistemasId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    SistemaNombre = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sistemas", x => x.SistemasId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sistemas");
+        }
+    }
+}
